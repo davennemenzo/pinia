515b6462ae763f3f79b4a6ba44c632f8abd04c23
davennemenzo <davennemenzo@gmail.com>
2024-08-26 01:10:22 +0800

    Pinia

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {useAuthStore} from "@/stores/auth";
+
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <header>
+    
+
+    <div class="wrapper ">
+      
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+
+
+const store = useCounterStore();
+console.log(store.count);
+</script>
+
+<template>
+  <main>
+   Home Page
+  </main>
+</template>
